4b70e35ed62e571d70e97f43e353435ca680d65f
Sand Man <rajeshbhadra62@gmail.com>
2023-07-31 00:44:44 +0530

    Back button press to generate bug, It is resolved 🪲

--- app/src/main/java/com/sharebysocial/com/Activities/HomeActivity.java
+++ app/src/main/java/com/sharebysocial/com/Activities/HomeActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationBarView;
 import com.sharebysocial.com.Fragment.AddFragment;
@@ -30,43 +31,50 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         helper = new Helper();
         fragmentManager = getSupportFragmentManager();
-        /* This function is going to use all helper class functions which are needed */
-        helperClass();
+        Helper.hideBar(this);
         /*In this Function we are Replacing Fragment*/
         replaceFragment();
         /*
          * Bottom navigation bar operation
-         *
          */
         bottomNavigation();
     }
 
     private void bottomNavigation() {
-        binding.bottomNavigationId.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                item.setCheckable(true);
-                if (item.getItemId() == R.id.home_btn) {
-                    helper.replaceFragment(fragmentManager, new HomeFragment(), R.id.containerId);
-                } else if (item.getItemId() == R.id.radar_btn) {
-                    helper.replaceFragment(fragmentManager, new RadarFragment(), R.id.containerId);
-                } else if (item.getItemId() == R.id.add_btn) {
-                    helper.replaceFragment(fragmentManager, new AddFragment(), R.id.containerId);
-                } else if (item.getItemId() == R.id.profile_btn) {
-                    helper.replaceFragment(fragmentManager, new ProfileFragment(), R.id.containerId);
-                } else if (item.getItemId() == R.id.history_btn) {
-                    helper.replaceFragment(fragmentManager, new HistoryFragment(), R.id.containerId);
-                }
-                return true;
+        binding.bottomNavigationId.setOnItemSelectedListener(item -> {
+            item.setCheckable(true);
+            if (item.getItemId() == R.id.home_btn) {
+                helper.replaceFragment(fragmentManager, new HomeFragment(), R.id.containerId);
+            } else if (item.getItemId() == R.id.radar_btn) {
+                helper.replaceFragment(fragmentManager, new RadarFragment(), R.id.containerId);
+            } else if (item.getItemId() == R.id.add_btn) {
+                helper.replaceFragment(fragmentManager, new AddFragment(), R.id.containerId);
+            } else if (item.getItemId() == R.id.profile_btn) {
+                helper.replaceFragment(fragmentManager, new ProfileFragment(), R.id.containerId);
+            } else if (item.getItemId() == R.id.history_btn) {
+                helper.replaceFragment(fragmentManager, new HistoryFragment(), R.id.containerId);
             }
+            return true;
         });
     }
+
 
     private void replaceFragment() {
         helper.replaceFragment(fragmentManager, new HomeFragment(), R.id.containerId);
     }
 
-    private void helperClass() {
-        helper.hideBar(this);
+    @Override
+    public void onBackPressed() {
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.bottomNavigationId);
+        if (currentFragment instanceof HomeFragment) {
+            // Handle back button press when in HomeFragment
+            // For example, navigate to a different fragment or exit the app
+            Toast.makeText(this, "back button press..", Toast.LENGTH_SHORT).show();
+            super.onBackPressed();
+        } else {
+            // If not in HomeFragment, navigate to HomeFragment on back button press
+            binding.bottomNavigationId.setSelectedItemId(R.id.home_btn);
+        }
     }
+
 }
